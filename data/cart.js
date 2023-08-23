184d0fd9a2f9aa17d00887f3e8dff14447392436
dipti <diptikotadiya@gmail.com>
2023-08-23 12:48:11 +0530

    display cartquantity in header and when delete cart update header

--- data/cart.js
+++ data/cart.js
@@ -51,3 +51,4 @@ export function removeFromCart(productId){
   cart = newCart;
   saveToStorage();
 }
+
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -107,14 +107,26 @@ document.querySelectorAll('.js-delete-link').
      const productId = link.dataset.productId;
      console.log(productId);
      removeFromCart(productId);
-     //console.log(cart);
+     console.log(cart);
 
     const container = document.querySelector(
       `.js-cart-item-container-${productId}`);
       //console.log(container);
       container.remove();
+      updateCartQuantity();
     });
   });
 
+function updateCartQuantity(){
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) =>{
+    cartQuantity += cartItem.quantity;
+  });
+  document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} Items`;
+}
+
+updateCartQuantity();
   
+
   
